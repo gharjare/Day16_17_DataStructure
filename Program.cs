6e087c097f5fe6fa28f74f188ac36b9d6ea9e1e7
gharjare <gharjarechetan@gmail.com>
2022-12-18 21:35:31 +0530

    add obj of prblm

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using DataStrucureAlgorithm;
+
+namespace DataStructureAlgorithm
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //BubbleSort bubbleSort = new BubbleSort();
+            //bubbleSort.Display();
+
+            //InsertionSort insertionSort = new InsertionSort();
+            //insertionSort.Display();
+
+            //Anagram anagram = new Anagram();
+            //anagram.Display();
+
+            //int[] arr = { 12, 11, 13, 5, 6, 7 };
+            //Console.WriteLine("Given Array");
+            //printArray(arr);
+            //MergeSort ob = new MergeSort();
+            //ob.sort(arr, 0, arr.Length - 1);
+            //Console.WriteLine("\nSorted array");
+            //printArray(arr);
+
+
+            //Console.WriteLine("Enter starting point : ");
+            //int start = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Ending Point : ");
+            //int end = Convert.ToInt32(Console.ReadLine());
+            //ArrayList primeNumbers = PrimeNumberRange.FindPrimeNumbers(start, end);
+            //Console.WriteLine($"\nAll the prime numbers between {start} and {end} are :");
+            //foreach (int prime in primeNumbers)
+            //{
+            //    Console.WriteLine(prime);
+            //}
+
+            // PalindromePrime obj = new PalindromePrime();
+            // for (int i = 1; i < 1000; i++)
+            // {
+            //     if (obj.isPrime(i) && obj.isPalindrome(i))
+            //         Console.WriteLine(i + " ");
+            // }
+
+           //OrderdList  list = new OrderdList ();
+           // list.addfirst(56);
+           // list.addfirst(40);
+           // list.addfirst(562);
+           // list.addfirst(4);
+           // list.addfirst(120);
+           // list.addfirst(78);
+           // Console.WriteLine("Before The Sorting");
+           // list.printlist();
+           // list.sortByNumber();
+           // Console.WriteLine("After The Sorting");
+           // list.printlist();
+
+
+
+        }
+    }
+    
+}
